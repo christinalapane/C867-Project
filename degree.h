384b41d513b95e4cc337752ab47cc0fe548a2207
Christina LaPane <70347084+christinalapane@users.noreply.github.com>
2021-11-09 10:42:44 -0500

    Create degree.h

---
+++ degree.h
@@ -0,0 +1,13 @@
+
+#ifndef degree_h
+#define degree_h
+#include <string>
+
+//Question C - create enum DegreeProgram//
+enum DegreeProgram{SECURITY, NETWORK, SOFTWARE};
+
+
+//array parallel to enum that displays string//
+static const std::string degreeProgramStrings[] = {"SECURITY", "NETWORK","SOFTWARE"};
+
+#endif /* degree_h */

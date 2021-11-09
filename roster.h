2c16f82f18f9fd43527c64c36de6771176bce702
Christina LaPane <70347084+christinalapane@users.noreply.github.com>
2021-11-09 10:48:34 -0500

    Update roster.h

--- roster.h
+++ roster.h
@@ -9,12 +9,13 @@ class Roster {
 private:
     int lastIndex = -1;
     const static int numStudents = 5;
-    Student* classRosterArray[numStudents];
+    
 
     public :
     
     //goes through a set of comma delimited strings and gathers data from student table//
     void parse(string longString);
+    Student* classRosterArray[numStudents];
     
     //All student data is put into add that sets the instance variables from stdent table and updates roster//
     void add(string StudentID,
@@ -32,7 +33,7 @@ private:
     //Question 3c. prints a complete tab-separated list of all info-code in roster.cpp//
     void printAll();
     //Question 3d. prints students average number of days in a course by studentID-code in roster.cpp//
-    void printAverageDaysInCourse();
+    void printAverageDaysInCourse(string StudentID);
     //question 3e. verifies student email. Should include @, ./ no space.-code in roster.cpp//
     void printInvalidEmails();
     //Question 3f. prints out student info for a degree program by enum.-code in roster.cpp//
